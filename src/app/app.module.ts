24aa6796fd8caee94efec1b67651244a4025e066
sheapa <patshea26@gmail.com>
2022-07-11 15:11:33 -0400

    half-way

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TasksComponent } from './Components/tasks/tasks.component';
+import { ButtonComponent } from './Components/button/button.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { AboutComponent } from './Components/about/about.component';
+import { TaskItemComponent } from './Components/task-item/task-item.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { AddTaskComponent } from './Components/add-task/add-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    ButtonComponent,
+    HeaderComponent,
+    AboutComponent,
+    TaskItemComponent,
+    FooterComponent,
+    AddTaskComponent
   ],
   imports: [
     BrowserModule
